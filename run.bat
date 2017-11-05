eb3a4d82647b036805f24be5a699a3568ac29e56
denis <ericrause@gmail.com>
2017-11-05 11:31:40 +0300

    compiler v0.1 is ready! =)

--- run.bat
+++ run.bat
@@ -2,10 +2,11 @@
 set /p file="enter your file: "
 "c:\Program Files\Java\jdk-9.0.1\bin\javac" %file%.java
 set /p execute="do you want to execute it? y/n "
-if %execute%=="y" goto execute_it
+if "%execute%"=="y" goto execute_it
 goto finish
 
 :execute_it
-call r_.bat %file%
+"c:\Program Files\Java\jdk-9.0.1\bin\java" %file%
+set /p wait=""
 
 :finish

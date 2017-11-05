dd772ef3526a8cdf8e9dff24fdd2a0b2ea31c48d
denis <ericrause@gmail.com>
2017-11-05 13:26:35 +0300

    work in progress. no comments

--- first.java
+++ first.java
@@ -1,3 +1,6 @@
+//------------------
+// its a SECOND task
+//------------------
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -5,16 +8,25 @@ import java.io.InputStreamReader;
 public class first { 
 	public static void main(String[] args) {
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   //creates new input reader "br"
         
-		System.out.println("please input 2(two) numubers: ");
-        String numbers = null;
+		System.out.println("please input numubers: ");
+        String numbers = null;              
+        
         try {
-            numbers = br.readLine();
+            numbers = br.readLine();        //writes string to numbers variable
         }
         catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace();            //took from internet, don't know what it prints
         }
         System.out.println("you've entered: " +numbers);
+        
+        //loop function add number
+        public class summarize {
+            public static void summ(int... args) {       //reads numbers and adds up them
+                System.out.println("functional " +numbers);
+            }
+            //first.summ(numbers);
+        }
 	}
 }
---
+++ second.java
@@ -0,0 +1,14 @@
+//------------------
+// its a FIRST task
+//------------------
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class first { 
+	public static void main(String[] args) {
+        system.out.println("first is " +args[0]);
+        system.out.println("second is " +args[1]);
+        
+    }
+}

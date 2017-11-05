5fbde4f1e887d623d2fce3cac8642687105bd7ef
denis <ericrause@gmail.com>
2017-11-05 13:48:20 +0300

    first task - second.java

--- second.java
+++ second.java
@@ -7,9 +7,10 @@ import java.io.InputStreamReader;
 
 public class second { 
 	public static void main(String[] args) {
-        System.out.println("first is " +args[0]);
-        System.out.println("second is " +args[1]);
+        
         try {
+            System.out.println("first is " +args[0]);
+            System.out.println("second is " +args[1]);
             int a1 = Integer.parseInt(args[0]);
             int a2 = Integer.parseInt(args[1]);
             int summ = a1 + a2;

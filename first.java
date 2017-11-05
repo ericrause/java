5cae1db41752226d177a12c4399b3b51cf9ee224
denis <ericrause@gmail.com>
2017-11-05 12:54:37 +0300

    added BufferedReader + InputStreamReader classes

--- first.java
+++ first.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class first { 
 	public static void main(String[] args) {
-		System.out.println("testing ");
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		System.out.println("please input 2(two) numubers: ");
+        String numbers = null;
+        try {
+            numbers = br.readLine();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("you've entered: " +numbers);
 	}
 }

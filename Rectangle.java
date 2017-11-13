c5b44c9bbdad514d057225a989573979defc2932
denis <ericrause@gmail.com>
2017-11-13 12:54:05 +0300

    something strange happened. whatever. development in progress

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,4 @@
-public interface ToCount {
+/** public interface ToCount {
     int getLength();
     
 }
@@ -8,4 +8,25 @@ public class Item implements ToCount {
         return 0;
     }
     
+}
+*/
+class Rectangle {
+    double width;
+    double height;
+    
+    double space() {
+        return width * height;
+    }
+}
+
+class Box1 {
+    public static void main(String args[]){
+        Rectangle mybox = new Rectangle();
+        double boxSpace;
+        mybox.width=5;
+        mybox.height=5;
+        boxSpace=mybox.space();
+        System.out.println("Space is" +boxSpace);
+        
+    }
 }

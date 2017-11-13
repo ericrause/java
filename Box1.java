b8c3240abb90cdbc4d8e0bb29bef9f29dbab93e0
denis <ericrause@gmail.com>
2017-11-13 13:37:04 +0300

    nothing works, but added get and set methods

--- Box1.java
+++ Box1.java
@@ -10,6 +10,11 @@ public class Item implements ToCount {
     
 }
 */
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 class Rectangle {
     double width;
     double height;
@@ -17,9 +22,42 @@ class Rectangle {
     double space() {
         return width * height;
     }
-    void setDimensions(double w, double h){
+    
+     public static String get_string(String str){
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+    }
+    
+    void getDims(double a1, double a2){
+            System.out.println("Please enter figure dimensions (a=? b=?)");
+            String str = null; 
+            str = get_string(str);
+            String spaces = "[ ]+";
+            String[] items = str.split(spaces);
+            double[] scalars;
+            int len = items.length; 
+
+            for (int i=0; i < len; i++)
+            try {
+                scalars[i]=Double.parseDouble(items[i]);
+            }
+            catch (Throwable t){
+                System.out.println("Please input only numubers");
+                System.exit(0);
+            }
+        
+    }
+    void setDims(double w, double h){
         width=w;
         height=h;
+    
     }
 }
 
@@ -28,7 +66,7 @@ class Box1 {
         Rectangle mybox = new Rectangle();
         double boxSpace;
         
-        mybox.setDimensions(5,5);
+        mybox.setDims(5,5);
             
         boxSpace=mybox.space();
         System.out.println("Space is " +boxSpace);

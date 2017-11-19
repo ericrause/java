ae8385071d207a350c640e8ed05d8c7097c46d52
ericrause <31223491+ericrause@users.noreply.github.com>
2017-11-19 13:46:24 +0300

    Add files via upload
    added textbox inputs

--- Calculator.java
+++ Calculator.java
@@ -21,6 +21,7 @@ class Calculator {
 
         //init container
         JFrame container = new JFrame("Hello kiity");
+        JTextField textBox = new JTextField(16);
         container.setSize(205,330);
         container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         container.setLayout(new FlowLayout());
@@ -41,6 +42,13 @@ class Calculator {
         JButton b9 = new JButton("9");
         JButton b0 = new JButton("0");
 
+        JButton bPlus = new JButton("+");
+        JButton bMinus = new JButton("-");
+        JButton bDiv = new JButton("/");
+        JButton bMul = new JButton("*");
+        JButton bDot = new JButton(".");
+        JButton bEq = new JButton("=");
+
 
 
         //better to use lambda
@@ -53,30 +61,46 @@ class Calculator {
         });
          */
         //button 1 pressed
-        b1.addActionListener( (ae) -> textLabel2.setText(b1.getText()));
-        b2.addActionListener( (ae) -> textLabel2.setText(b2.getText()));
-        b3.addActionListener( (ae) -> textLabel2.setText(b3.getText()));
-        b4.addActionListener( (ae) -> textLabel2.setText(b4.getText()));
-        b5.addActionListener( (ae) -> textLabel2.setText(b5.getText()));
-        b6.addActionListener( (ae) -> textLabel2.setText(b6.getText()));
-        b7.addActionListener( (ae) -> textLabel2.setText(b7.getText()));
-        b8.addActionListener( (ae) -> textLabel2.setText(b8.getText()));
-        b9.addActionListener( (ae) -> textLabel2.setText(b9.getText()));
-        b0.addActionListener( (ae) -> textLabel2.setText(b0.getText()));
+        b1.addActionListener( (ae) -> textBox.setText(textBox.getText() + b1.getText()));
+        b2.addActionListener( (ae) -> textBox.setText(textBox.getText() + b2.getText()));
+        b3.addActionListener( (ae) -> textBox.setText(textBox.getText() + b3.getText()));
+        b4.addActionListener( (ae) -> textBox.setText(textBox.getText() + b4.getText()));
+        b5.addActionListener( (ae) -> textBox.setText(textBox.getText() + b5.getText()));
+        b6.addActionListener( (ae) -> textBox.setText(textBox.getText() + b6.getText()));
+        b7.addActionListener( (ae) -> textBox.setText(textBox.getText() + b7.getText()));
+        b8.addActionListener( (ae) -> textBox.setText(textBox.getText() + b8.getText()));
+        b9.addActionListener( (ae) -> textBox.setText(textBox.getText() + b9.getText()));
+        b0.addActionListener( (ae) -> textBox.setText(textBox.getText() + b0.getText()));
+
+        bPlus.addActionListener( (ae) ->    textBox.setText(textBox.getText() + bPlus.getText()));
+        bMinus.addActionListener( (ae) ->   textBox.setText(textBox.getText() + bMinus.getText()));
+        bDiv.addActionListener( (ae) ->     textBox.setText(textBox.getText() + bDiv.getText()));
+        bMul.addActionListener( (ae) ->     textBox.setText(textBox.getText() + bMul.getText()));
+        bDot.addActionListener( (ae) ->     textBox.setText(textBox.getText() + bDot.getText()));
+        bEq.addActionListener( (ae) ->      textBox.setText(textBox.getText() + bEq.getText()));
 
 
         //adding components
-        container.add(textLabelGreetings);
-        container.add(b1);
-        container.add(b2);
-        container.add(b3);
-        container.add(b4);
-        container.add(b5);
-        container.add(b6);
-        container.add(b7);
-        container.add(b8);
+        container.add(textBox);
         container.add(b9);
+        container.add(b8);
+        container.add(b7);
+        container.add(bPlus);
+
+        container.add(b6);
+        container.add(b5);
+        container.add(b4);
+        container.add(bMinus);
+
+        container.add(b3);
+        container.add(b2);
+        container.add(b1);
+        container.add(bDiv);
+
+        container.add(bMul);
         container.add(b0);
+        container.add(bDot);
+        container.add(bEq);
         container.add(textLabel2);
 
         //show container
